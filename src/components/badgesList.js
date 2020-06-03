2ade537be81d41978af7fa1f0022df885fbef02d
Jorge Almonacid <jorgealmonacid24@gmail.com>
2020-06-02 20:11:01 -0400

    Added filter search

--- src/components/badgesList.js
+++ src/components/badgesList.js
@@ -26,11 +26,36 @@ class BadgesListItem extends React.Component {
   }
 }
 
-class BadgesList extends React.Component {
-  render() {
-    if (this.props.badges.length === 0) {
+
+function useSearchBadges(badges) {
+  const [query, setQuery] = React.useState('');
+  const [filteredBadges, setFilteredBadges] = React.useState(badges);
+
+  React.useMemo(() => {
+    const result = badges.filter(badge => {
+      return `${badge.firstName} ${badge.lastName}`
+        .toLowerCase()
+        .includes(query.toLowerCase());
+    });
+
+    setFilteredBadges(result);
+  }, [badges, query]);
+
+  return { query, setQuery, filteredBadges };
+}
+
+function BadgesList(props) {
+  const {badges} = props;
+  const { query, setQuery, filteredBadges } = useSearchBadges(badges);
+
+    if (filteredBadges.length === 0) {
       return (
         <div>
+          <div className="form-group">
+            <label>Filter Badges</label>
+            <input type="text" className="form-control" value={query} onChange={e => {
+              setQuery(e.target.value)}}/>
+          </div>
           <h3>No badges were found</h3>
           <Link className="btn btn-primary" to="/badges/new">
             Create new badge
@@ -41,8 +66,13 @@ class BadgesList extends React.Component {
 
     return (
       <div className="BadgesList">
+        <div className="form-group">
+          <label>Filter Badges</label>
+          <input type="text" className="form-control" value={query} onChange={e => {
+            setQuery(e.target.value)}}/>
+        </div>
         <ul className="list-unstyled">
-          {this.props.badges.map(badge => {
+          {filteredBadges.map(badge => {
             return (
               <li key={badge.id}>
                 <Link className="text-reset text-decoration-none" to={`/badges/${badge.id}`}>
@@ -55,6 +85,5 @@ class BadgesList extends React.Component {
       </div>
     );
   }
-}
 
 export default BadgesList;
